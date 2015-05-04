d5d4ecfa67edcddefe04c76696264db061788aee
Xinjiang Shao <shaoxinjiang@gmail.com>
2015-05-04 14:13:30 -0500

    Added all brew tasks, brew cask tasks and dotfiles I will be using

---
+++ README.md
@@ -0,0 +1,10 @@
+# Xinjiang's dotfiled
+
+The dotfiles are based on paulirish (https://github.com/paulirish/dotfiles/)
+
+# Tools I use
+
+- Sublime Text 3 
+- Home Brew
+- Brew Cask for installing binaries
+- Zsh
---
+++ bootstrap.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# copy paste this file in bit by bit.
+# don't run it.
+  echo "do not run this script in one go. hit ctrl-c NOW"
+  read -n 1
+sudo -v
+##
+## new machine setup.
+##
+
+
+
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+#
+# install all the things
+sh brew
+sh brew-cask
+
+# github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+bash < <( curl https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh)
+
+# Type `git open` to open the GitHub page or website for a repository.
+npm install -g git-open
+
+# github.com/rupa/z   - oh how i love you
+git clone https://github.com/rupa/z.git ~/code/z
+chmod +x ~/code/z/z.sh
+
+
+
+# github.com/thebitguru/play-button-itunes-patch
+# disable itunes opening on media keys
+git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
+
+# change to bash 4 (installed by homebrew)
+BASHPATH=$(brew --prefix)/bin/bash
+sudo echo $BASHPATH >> /etc/shells
+chsh -s $BASHPATH # will set for current user only.
+echo $BASH_VERSION # should be 4.x not the old 3.2.X
+# Later, confirm iterm settings aren't conflicting.
+
+# go read mathias, paulmillr, gf3, alraa's dotfiles to see what to update with.
+
+# set up osx defaults
+#   maybe something else in here https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
+#sh osx
+
+# Install oh-my-zsh now
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+# Install antigen
+curl https://raw.github.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh
+source antigen.zsh
+
+
+# symlinks!
+#   put/move git credentials into ~/.gitconfig.local
+#   http://stackoverflow.com/a/13615531/89484
+sh symlink-setup.sh
+
